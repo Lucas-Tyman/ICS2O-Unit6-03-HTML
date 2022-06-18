d682e563e0c2787f2c687c685c4a3e29ed0cf1e9
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-06-18 01:49:31 +0000

    6

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // Created by: Lucas Tyman
-// Created on: Apr 2022
+// Created on: May 2022
 // This file contains the JS functions for index.html
+
+'use strict'
 
 /**
  * Check service worker.
@@ -12,8 +14,22 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+ * Get API for weather.
+*/
+const getWeather = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData.main.temp)
+    const temperature = jsonData.main.temp - 273.15
+    const symbol = jsonData.weather[0].icon
+    const description = jsonData.weather[0].description
+    document.getElementById("api-weather").innerHTML = "The current weather is " + temperature.toFixed(0) + "°C" + "<img src='https://openweathermap.org/img/wn/" + symbol + "@2x.png' alt='Weather Icon' width='15%'>" + "with " + description
+  } catch (err) {
+    console.log(err)
+    document.getElementById("api-weather").innerHTML = "Error fetching current weather."
+  }
 }
+
+
+getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
